ab3418a7b3fe1fd4151a48e91174b5b3fa6bf059
Caduazevedofreitas <barucks123@gmail.com>
2025-07-17 01:07:42 +0000

    nivel  mestre

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,75 @@
 #include <stdio.h>
 
-int main() {
-    int i;
-
-    printf("Movimento da Torre:\n");
-    for (i = 1; i <= 5; i++) {
+void moverTorre(int casas) {
+    if (casas == 0) return;
     printf("Direita\n");
-    }
+    moverTorre(casas - 1);
+}
 
-    printf("Movimento do Bispo:\n");
-    i = 1;
-    while (i <= 5) {
+
+void moverBispoRecursivo(int casas) {
+    if (casas == 0) return;
     printf("Cima Direita\n");
-        i++;
-    }
+    moverBispoRecursivo(casas - 1);
+}
 
-    printf("Movimento da Rainha:\n");
-    i = 1;
-    do {
+
+void moverRainha(int casas) {
+    if (casas == 0) return;
     printf("Esquerda\n");
-        i++;
-    } while (i <= 8);
+    moverRainha(casas - 1);
+}
 
+
+void moverCavalo() {
     printf("\nMovimento do Cavalo:\n");
 
-    int Baixo = 2;
-    int Esquerda = 1;
-
+    const int tamanhoTabuleiro = 6;
+    for (int x = 0; x < tamanhoTabuleiro; x++) {
+        for (int y = 0; y < tamanhoTabuleiro; y++) {
+            if (x - 2 >= 0 && y + 1 < tamanhoTabuleiro) {
     
-    for (i = 1; i <= Baixo; i++) {
-        printf("Baixo\n");
+                if ((x + y) % 2 == 0) continue;
 
-    
-    if (i == Baixo) {
-            int j = 1;
-    while (j <= Esquerda) {
-         printf("Esquerda\n");
-                j++;
+                printf("Cavalo de (%d,%d) para (%d,%d): Cima-Cima-Direita\n", x, y, x - 2, y + 1);
+            }
+
+            if (x == 4 && y == 4) break; 
+        }
+    }
+}
+
+
+void moverBispoComLoop() {
+    printf("\nMovimento do Bispo com Loops Aninhados:\n");
+
+    for (int i = 1; i <= 4; i++) { 
+        for (int j = 1; j <= 4; j++) { 
+            if (i == j) {
+                printf("Diagonal (%d,%d): Cima Direita\n", i, j);
             }
         }
     }
+}
+
+int main() {
+    
+    int casasTorre = 5;
+    int casasBispo = 5;
+    int casasRainha = 8;
+
+    printf("Movimento da Torre:\n");
+    moverTorre(casasTorre);
+
+    printf("\nMovimento do Bispo (Recursivo):\n");
+    moverBispoRecursivo(casasBispo);
+
+    printf("\nMovimento da Rainha:\n");
+    moverRainha(casasRainha);
+
+    moverCavalo();
+
+    moverBispoComLoop();
 
     return 0;
-}   
+}
